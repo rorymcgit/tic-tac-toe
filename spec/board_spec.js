c4397eb47f811f223dd769486cca8c56c51b4d82
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-04-20 11:32:52 +0100

    Add squares array to board

--- spec/board_spec.js
+++ spec/board_spec.js
@@ -5,5 +5,9 @@ describe('Board', function() {
      expect(board).toBeDefined();
   });
 
-  
+  it("has an array containing all 9 possible squares", function() {
+    expect(board.squares).toEqual(["L1", "M1", "R1",
+                                   "L2", "M2", "R2",
+                                   "L3", "M3", "R3"]);
+  });
 });
--- src/board.js
+++ src/board.js
@@ -1,7 +1,9 @@
 (function(exports) {
 
   function Board() {
-
+    this.squares = ["L1", "M1", "R1",
+                  "L2", "M2", "R2",
+                  "L3", "M3", "R3"];
   }
 
   exports.Board = Board;
